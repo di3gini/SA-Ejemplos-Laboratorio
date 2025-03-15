ef63ddebac40eff9cbd4bd3b4e4fc2a5de9deaa9
Diego Molina <di3gini3094@gmail.com>
2025-03-14 19:20:00 -0600

    Working example

---
+++ graphql-example/src/example_data/books.js
@@ -0,0 +1,34 @@
+export const booksData = [
+    {
+      title: 'Harry Potter and the Chamber of Secrets',
+      author: 'J.K. Rowling',
+      published_at: '1998-07-02',
+    }, 
+    {
+      title: 'Harry Potter and the Prisoner of Azkaban',
+      author: 'J.K. Rowling',
+      published_at: '1999-07-08',
+    },
+    {
+      title: 'Harry Potter and the Goblet of Fire',
+      author: 'J.K. Rowling',
+      published_at: '2000-07-08',
+    },
+    {
+      title: 'Lord of the Rings',
+      author: 'J.R.R. Tolkien',
+      published_at: '1954-07-29',
+    },
+    {
+      title: 'Clean Code',
+      author: 'Robert C. Martin',
+      published_at: '2008-08-11',
+    },
+    {
+      title: 'La metamorfosis de Kafka',
+      author: 'Franz Kafka',
+      published_at: '1915-10-15',
+    }
+];
+
+export const books = booksData;
